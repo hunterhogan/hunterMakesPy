0f96bf4d07b690156dd2cb66d491a8561460d3f5
Hunter Hogan <hunterhogan@users.noreply.github.com>
2026-03-18 22:48:13 -0500

    GitHub Actions.

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import Extension, setup
-
-setup(ext_modules = [
-	Extension(name = "humpy_cytoolz.dicttoolz", sources = ["humpy_cytoolz/dicttoolz.pyx"]),
-	Extension(name = "humpy_cytoolz.functoolz", sources = ["humpy_cytoolz/functoolz.pyx"]),
-	Extension(name = "humpy_cytoolz.itertoolz", sources = ["humpy_cytoolz/itertoolz.pyx"]),
-	Extension(name = "humpy_cytoolz.recipes", sources = ["humpy_cytoolz/recipes.pyx"]),
-	Extension(name = "humpy_cytoolz.utils", sources = ["humpy_cytoolz/utils.pyx"]),
-])
